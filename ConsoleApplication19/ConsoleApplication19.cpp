b07b2b0ed9bc3b9360a172e5ba8a08126f8b968b
Damir <Damir@DESKTOP-ELD43TT>
2024-04-10 01:51:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Employee {
+    string lastName;
+    int employeeNumber;
+    int birthMonth;
+
+    void printInfo() const {
+        cout << "Last name: " << lastName << ", Number: " << employeeNumber << ", Month of birthday: " << birthMonth << endl;
+    }
+};
+
+int main() {
+    const int numEmployees = 3; 
+
+    vector<Employee> employees = {
+        {"Kennedy", 1, 5},
+        {"Clinton", 2, 7},
+        {"Nixon", 3, 9}
+    };
+
+    cout << "Full list of employees: " << endl;
+    for (const auto& employee : employees) {
+        employee.printInfo();
+    }
+
+    int requestedMonth;
+    cout << "\nEnter the month number to display employees born in this month (1-12): ";
+    cin >> requestedMonth;
+
+    cout << "\nEmployees who were born in the month " << requestedMonth << ":" << endl;
+    for (const auto& employee : employees) {
+        if (employee.birthMonth == requestedMonth) {
+            employee.printInfo();
+        }
+    }
+
+    return 0;
+}
